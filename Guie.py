68e6ed95e36f588d256295d64d15cea6a9301cdb
Jhony <pieroespinosaedquen@gmail.com>
2025-04-28 16:26:12 -0500

    Coment deleyte coment

--- Guie.py
+++ Guie.py
@@ -1,4 +1,4 @@
 # Guia de git y github
 print("Hola mundo desde Python")
 print("Hola git y github 11111")
-#esto es un comentario que se va a ir borrando
+#esto es un comentario que se va a ir borrando en el futuro
