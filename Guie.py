b18871f1981858c298fb1670a7aca8003327ab27
Etu-Fashin <sanchezfigueroajosemariano@gmail.com>
2025-04-28 17:12:26 -0500

    Update Guie.py
    Tutorial

--- Guie.py
+++ Guie.py
@@ -1,8 +1,4 @@
 # Guia de git y github
 print("Hola mundo desde Python")
 print("Hola git y github 11111")
-<<<<<<< HEAD
-#esto es un comentario que se va a ir borrando en el futuro
-=======
 #esto es un comentario que se va a ir borrando
->>>>>>> 148a012 (Coment deleyte)
