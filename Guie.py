9b137b4abc369d0522328d1d3c1ff75f9bdbec81
Jhony <pieroespinosaedquen@gmail.com>
2026-04-22 17:45:42 -0500

    añadiendo cosas

--- Guie.py
+++ Guie.py
@@ -1,8 +1,4 @@
 # Guia de git y github
 print("Hola mundo desde Python")
 print("Hola git y github 11111")
-#esto es un comentario que se va a ir borrando
-<<<<<<< HEAD
-
-=======
->>>>>>> b18871f1981858c298fb1670a7aca8003327ab27
+#esto es un comentario que se va a ir borrando en el futuro
--- Guie2.py
+++ Guie2.py
@@ -1,3 +1,4 @@
 # Guia de git y github
 print("Hola mundo desde Python 2")
 print("Hola git y github 2")
+#esto es un comentario que se va a ir borrando en el futuro
