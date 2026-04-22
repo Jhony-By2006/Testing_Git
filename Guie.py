182dbf48f556f5aa112d9a9d2da0e2c60e278a66
LAPTOP-VBENKGMI\etuwu <etuwuachin@gmail.com>
2026-04-22 17:54:32 -0500

    Un commint para oscar GAAA. es el primer commint mio

--- Guie.py
+++ Guie.py
@@ -2,7 +2,5 @@
 print("Hola mundo desde Python")
 print("Hola git y github 11111")
 #esto es un comentario que se va a ir borrando
-<<<<<<< HEAD
 
-=======
->>>>>>> b18871f1981858c298fb1670a7aca8003327ab27
+#Oscar pa cuando te bajas un juego pirata
