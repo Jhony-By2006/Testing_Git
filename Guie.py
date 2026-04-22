c05664b0b5ed335c017bd12326e9ff29d223f8d7
Jhony <pieroespinosaedquen@gmail.com>
2026-04-22 18:28:20 -0500

    añadiendo nuevo comentario

--- Guie.py
+++ Guie.py
@@ -1,14 +1,8 @@
 # Guia de git y github
 print("Hola mundo desde Python")
 print("Hola git y github 11111")
-<<<<<<< HEAD
-<<<<<<< HEAD
-#esto es un comentario que se va a ir borrando en el futuro
-=======
-#esto es un comentario que se va a ir borrando en el futuro
->>>>>>> main
-=======
 #esto es un comentario que se va a ir borrando
 
 #Oscar pa cuando te bajas un juego pirata
->>>>>>> EtuFashin
+
+#Oscar es mi esclavo y no lo niega terrible
