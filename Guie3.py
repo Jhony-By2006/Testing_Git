a0d93c874c6e0460db687919a0cb0bcc86ca8ba8
Jhony <pieroespinosaedquen@gmail.com>
2026-04-22 19:12:33 -0500

    all int

--- Guie3.py
+++ Guie3.py
@@ -4,13 +4,6 @@ print ("Hola mundo xDDDDDDDD")
 
 #Oscar traes la laptop
 #Mariano pto cuando te bajas un juego pirata
-<<<<<<< HEAD
 #seprandome a mi rama master
-=======
 #Ander es falla
 #Oscar activa las manos
-<<<<<<< HEAD
->>>>>>> cb7b39d1a6456a0e418ae11d6b09d8c59259ef59
-=======
->>>>>>> origin/EtuFashin
->>>>>>> master
