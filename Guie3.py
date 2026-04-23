43e52517d5b8a9b1d7415c3bf6162c0f4da4fe7d
Jhony <pieroespinosaedquen@gmail.com>
2026-04-22 19:03:21 -0500

    Revert "Merge remote-tracking branch 'origin/EtuFashin'"
    This reverts commit 9a9cc396977bde5f6d54ba442f44553a73fc06b9, reversing
    changes made to 72939b1e4fbf51ab0c6ddbe626a434f9fca59968.

--- Guie3.py
+++ Guie3.py
@@ -4,9 +4,4 @@ print ("Hola mundo xDDDDDDDD")
 
 #Oscar traes la laptop
 #Mariano pto cuando te bajas un juego pirata
-<<<<<<< HEAD
 #seprandome a mi rama master
-=======
-#Ander es falla
-#Oscar activa las manos
->>>>>>> origin/EtuFashin
